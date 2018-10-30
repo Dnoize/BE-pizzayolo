d8c608881932fbc910551c4aed8d72e76e9ef12f
Damien Dechef <damien.dechef@gmail.com>
2018-10-30 17:13:18 +0100

    back end pour la page notre carte effectué

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const Mongoose = require("mongoose");
 const Ingredient = require("./models/ingredient");
-const Suggestion = require("./models/suggestion");
 const Pizza = require("./models/pizza");
 
 const Http = require("http");
@@ -41,52 +40,3 @@ function getIngredientsById(ingredientsArr, id) {
     return item.id == id;
   });
 }
-
-// .exec(function(err,pizza){
-//     if (err) return handleError(err);
-//     console.log(pizza[0]);
-//    })
-
-// Pizza.find({}, { _id: 0, name: 1, ingredients: 1, price: 1 }, (error, pizzas) => {
-
-//     const return_pizzas = [];
-//     pizzas.map(item => {
-//         let ingredientsPizza = [];
-//         let promises = []
-//         item.ingredients.map(item2 => {
-//             promises.push(Ingredient.find({ id_ing: item2 }))
-//         })
-//         Promise.all(promises).then(data => {
-//             ingredientsPizza.push(data)
-//             // console.log(ingredientsPizza)
-//             item.ingredients = ingredientsPizza;
-//             console.log(return_pizzas)
-
-//         })
-
-//     })
-
-//     res.json(pizzas)
-
-// });
-
-// Router.get('/suggestions', (req, res) => {
-//     Suggestion.find({}, { _id: 0, name: 1, ingredients: 1 }, (error, suggestions) => {
-//         suggestions.forEach(element => {
-//             console.log(element);
-//             let query = [];
-//             console.log(element.name)
-//             console.log(element.ingredients);
-//             element.ingredients.forEach(elementdeux => {
-//                 query.push({ 'id.ing': elementdeux });
-//                 console.log(query);
-//             });
-//             query = { $or: query }
-//             Ingredient.find(query, { _id: 0, name: 1 }, (error, IngSug) => {
-//                 console.log(IngSug);
-//                 suggestions["IngredientName"] = IngSug;
-//             })
-//         });
-//         res.json(suggestions)
-//     })
-// })
